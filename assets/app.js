4463b5be2c18ecf7ac2fb33780b362a48c8c451c
bruce.slaughter <bruce@ns.expert>
2022-04-06 21:25:27 -0400

    add site

---
+++ assets/app.js
@@ -0,0 +1,21 @@
+
+$(document).ready(function(){
+    $("#testimonial-slider").owlCarousel({
+        items:1,
+        itemsDesktop:[1000,1],
+        itemsDesktopSmall:[979,1],
+        itemsTablet:[768,1],
+        pagination:true,
+        navigation:true,
+        navigationText:["",""],
+        slideSpeed:1000,
+        autoPlay:true
+    });
+});
+
+$(function () {
+    $(document).scroll(function () {
+      var $nav = $(".navbar");
+      $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+    });
+  });
